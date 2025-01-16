def1c9e9b1ca4102c3c6318575e320e91c94189e
Anish Thapa Magar <116520964+Cleorte@users.noreply.github.com>
2025-01-16 17:13:41 -0500

    QA

--- portfolio/src/app/extras/page.tsx
+++ portfolio/src/app/extras/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+
+import { ReactTyped } from "react-typed";
+
 const Extras = () => {
-    return(
-        <div>
-            Extra Content
+    return (
+        <div className="text-center">
+            <div className='text-6xl font-extrabold sm:mt-10'>
+                {/* Main Text */}
+                Extra Componenet is
+                <div className='mt-5 pb-2 text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-teal-500 to-emerald-500'>
+                    Coming Soon
+                    <div>
+                        <ReactTyped
+                            strings={[
+                                "...",
+                            ]}
+                            loop={true}
+                            typeSpeed={90}
+                            backSpeed={30}
+                            showCursor={false}
+                        />
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
